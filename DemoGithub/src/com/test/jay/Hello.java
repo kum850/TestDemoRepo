5461dc117b601ae47d40aeadf56cc64d68744394
Jayprakash <Jayprakash@DESKTOP-C1KOD4K>
2021-12-15 03:44:41 -0800

    Committing new class

---
+++ DemoGithub/src/com/test/jay/Hello.java
@@ -0,0 +1,13 @@
+package com.test.jay;
+
+public class Hello {
+
+	public static void main(String[] args) {
+		int a=20;
+		int b=5;
+		int c=a-b;
+		System.out.println("addition of tw0 value"+c);
+
+	}
+
+}
